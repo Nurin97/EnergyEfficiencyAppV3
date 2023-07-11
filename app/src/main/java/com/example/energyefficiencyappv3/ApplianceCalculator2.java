5e06a6c9d1e9b5aa8f94974987dc58281eb3f288
rasid maasom <rasid.maasom@gmail.com>
2023-07-12 00:30:44 +0800

    The second commit..

--- app/src/main/java/com/example/energyefficiencyappv3/ApplianceCalculator2.java
+++ app/src/main/java/com/example/energyefficiencyappv3/ApplianceCalculator2.java
@@ -17,6 +17,8 @@ public class ApplianceCalculator2 extends AppCompatActivity implements View.OnCl
     private final ImageButton[] removeButtons = new ImageButton[5];
     private final TextView[] quantityTextViews = new TextView[5];
 
+    private String selectedAppliance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,30 +53,42 @@ public class ApplianceCalculator2 extends AppCompatActivity implements View.OnCl
         calculateButton.setOnClickListener(this);
     }
 
+
+
     @Override
     public void onClick(View v) {
         int id = v.getId();
         int index = -1;
         if (id == R.id.add1) {
             index = 0;
+            selectedAppliance = "Computer";
         } else if (id == R.id.add2) {
             index = 1;
+            selectedAppliance = "Air Conditioner";
         } else if (id == R.id.add3) {
             index = 2;
+            selectedAppliance = "TV";
         } else if (id == R.id.add4) {
             index = 3;
+            selectedAppliance = "Vacuum";
         } else if (id == R.id.add5) {
             index = 4;
+            selectedAppliance = "Printer";
         } else if (id == R.id.remove1) {
+            selectedAppliance = "Computer";
             index = 0;
         } else if (id == R.id.remove2) {
             index = 1;
+            selectedAppliance = "Air Conditioner";
         } else if (id == R.id.remove3) {
             index = 2;
+            selectedAppliance = "TV";
         } else if (id == R.id.remove4) {
             index = 3;
+            selectedAppliance = "Vacuum";
         } else if (id == R.id.remove5) {
             index = 4;
+            selectedAppliance = "Printer";
         } else if (id == R.id.calculateButton) {
             calculateTotalPowerConsumption();
         }
@@ -103,14 +117,29 @@ public class ApplianceCalculator2 extends AppCompatActivity implements View.OnCl
         Intent intent = new Intent(ApplianceCalculator2.this, LivingroomResult.class);
         intent.putExtra("totalPowerConsumption", totalPowerConsumption);
         startActivity(intent);
-
     }
 
     private int calculatePowerConsumption() {
         int totalPowerConsumption = 0;
         // Calculate the total power consumption based on the quantities and appliance types
         // Implement your logic here to calculate the power consumption for each appliance
-
+        switch (selectedAppliance) {
+            case "Computer":
+                totalPowerConsumption = quantities[0] * 100; // Assuming each refrigerator consumes 100 watts
+                break;
+            case "Air Conditioner":
+                totalPowerConsumption = quantities[1] * 50; // Assuming each toaster consumes 50 watts
+                break;
+            case "TV":
+                totalPowerConsumption = quantities[2] * 200; // Assuming each rice cooker consumes 200 watts
+                break;
+            case "Vacuum":
+                totalPowerConsumption = quantities[3] * 800; // Assuming each microwave consumes 800 watts
+                break;
+            case "Printer":
+                totalPowerConsumption = quantities[4] * 800; // Assuming each printer consumes 800 watts
+                break;
+        }
         return totalPowerConsumption;
     }
 }
